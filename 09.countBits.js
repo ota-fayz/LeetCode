7be276489d1be6dcb8afd554aa9f1563cb2e0311
Otabek Fayziev <ota_fayz@MacBook-Pro-Otabek.local>
2022-05-27 11:52:14 +0500

    8 9 done

---
+++ 09.countBits.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+
+function fromDecimalToBinary(n) {
+    let binaries = []
+    let reminder
+    let num = n
+    do {
+        reminder = num % 2
+        num = Math.floor(num / 2)
+        binaries.push(reminder)
+    } while (num !== 0)
+    binaries.reverse()
+    return binaries
+}
+
+function countOnes(arr) {
+    let sumOnes = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            sumOnes += 1
+        }
+    }
+    return sumOnes
+}
+
+var countBits = function (n) {
+    let ans = []
+    for (let i = 0; i <= n; i++) {
+        let binary = fromDecimalToBinary(i)
+        ans.push(countOnes(binary))
+    }
+    console.log(ans)
+    return ans
+}
+
+countBits(2)
+countBits(5)
+
