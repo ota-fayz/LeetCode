de981bc3097835bcf4dc2b3f71d3cf06960757d9
Otabek Fayziev <ota_fayz@MacBook-Pro-Otabek.local>
2022-06-05 04:23:01 +0500

    304 fast done

---
+++ leetcode problems/51.solveNQueens.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+    let matrix = []
+    let ans = []
+
+    for (let i=0; i<n; i++) {
+        matrix[i] = new Array(n)
+    }
+
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<n; j++) {
+
+        }
+    }
+    console.log(matrix)
+    return matrix
+};
+
+solveNQueens(4)
+// solveNQueens(1)
