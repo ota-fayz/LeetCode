c9405957bbc388b2f7ffc933b448b5500840ddcf
Otabek Fayziev <ota_fayz@MacBook-Pro-Otabek.local>
2022-06-15 16:43:42 +0500

    1048

--- leetcode problems/1048.longestStrChain.js
+++ leetcode problems/1048.longestStrChain.js
@@ -3,39 +3,33 @@
  * @return {number}
  */
 var longestStrChain = function (words) {
-  const sortArr = words.sort((a, b) => a.length - b.length);
-  const map = new Map();
-  map.set(sortArr[0], sortArr[0]);
+    words.sort((a, b) => a.length - b.length)
 
-  for (let i = 1, j = 2; i < sortArr.length - 1; i++) {
-    let p1 = sortArr[i],
-      p2 = sortArr[j];
+    let dp = []
+    let max = 1
 
-    if (p1.length === Array.from(map.values()).pop().length + 1) {
-      for (let i = 0; i < p1.length; i++) {
-        const newStr = p1.slice(0, i) + p1.slice(i + 1);
-        if (Array.from(map.values()).pop() === newStr) {
-          map.set(p1, p1);
-          break;
+    for (let i = 0; i < words.length; i++) dp[i] = 1
+
+    for (let i = 1; i < words.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (isPredecessor(words[j], words[i])) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+                max = Math.max(max, dp[i])
+            }
         }
-      }
     }
 
-    if (p2.length === Array.from(map.values()).pop().length + 1) {
-      for (let i = 0; i < p2.length; i++) {
-        const newStr = p2.slice(0, i) + p2.slice(i + 1);
-        if (Array.from(map.values()).pop() === newStr) {
-          map.set(p1, p1);
-          break;
-        }
-      }
+    return max
+}
+
+function isPredecessor(word1, word2) {
+    for (let i = 0; i < word2.length; i++) {
+        if (word1 === word2.substring(0, i) + word2.substring(i + 1, word2.length)) return true
     }
 
-    j++;
-  }
-  console.log(map);
-};
+    return false
+}
 
-longestStrChain(["a", "b", "ba", "bca", "bda", "bdca"]);
-// longestStrChain(["xbc", "pcxbcf", "xb", "cxbc", "pcxbc"]);
-// longestStrChain(["abcd", "dbqca"]);
+longestStrChain(["a", "b", "ba", "bca", "bda", "bdca"])
+longestStrChain(["xbc", "pcxbcf", "xb", "cxbc", "pcxbc"])
+longestStrChain(["abcd", "dbqca"])
